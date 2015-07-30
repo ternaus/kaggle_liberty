8fc152dfb90a80d542a0bdba48d41c7b7d000239
Vladimir Iglovikov <iglovikov@gmail.com>
2015-07-30 07:58:06 -0700

    First try with 5 fold CV for linear merging

--- src/find_weights_cv.py
+++ src/find_weights_cv.py
@@ -9,6 +9,7 @@ except:
 import sys
 sys.path += ['Predict']
 from sklearn.linear_model import LinearRegression
+from sklearn import cross_validation
 
 try:
   from src.Predict import XGB
@@ -27,7 +28,6 @@ __author__ = 'Vladimir Iglovikov'
 
 '''
 This script will use cross validation to estimate weights for merging of the different models.
-(I will try with hold out set first)
 '''
 
 import pandas as pd
@@ -38,7 +38,6 @@ random_state = 42
 
 xgb_test = pd.read_csv('predictions1/1438258857.28.csv')
 nn_test = pd.read_csv('predictions/1438258912.82.csv')
-
 
 '''
 [1] read train data and cut hold out set out of it
@@ -51,26 +50,43 @@ train = joined[joined['Hazard'] != -1]
 y = train['Hazard']
 X = train.drop(['Id', 'Hazard'], 1)
 
-#cut holdout set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
+kf = cross_validation.KFold(len(y), n_folds=5, random_state=random_state)
 
-'''
-[2] Do XGB and NN simulation on the train set and create prediction on the hold out set.
-'''
+result_nn = pd.DataFrame()
+result_xgb = pd.DataFrame()
 
-nn_prediction = NN.NN(X_train, y_train, X_test, y_test, uselog=True)
-xgb_prediction = XGB.XGB(X_train, y_train, X_test, y_test, uselog=True)
+ind = 0
+for train_index, test_index in kf:
+  ind += 1
+  X_train = X[train_index]
+  y_train = y[train_index]
+
+  X_test = X[test_index]
+  y_test = y[test_index]
+
+  '''
+  [2] Do XGB and NN simulation on the train set and create prediction on the hold out set.
+  '''
+
+  nn_prediction = NN.NN(X_train, y_train, X_test, y_test, uselog=True)
+  xgb_prediction = XGB.XGB(X_train, y_train, X_test, y_test, uselog=True)
 
 
-# print np.reshape(nn_prediction, len(nn_prediction))
-'''
-[3] Merge previous predictions into dataset and do linear regression on it
-'''
+  # print np.reshape(nn_prediction, len(nn_prediction))
+  '''
+  [3] Merge previous predictions into dataset and do linear regression on it
+  '''
 
-result_train = pd.DataFrame()
+  result_train = pd.DataFrame()
 
-result_train['nn'] = nn_prediction
-result_train['xgb'] = xgb_prediction
+  result_nn[ind] = nn_prediction
+  result_xgb[ind] = xgb_prediction
+
+  result_train['nn'] = result_nn.mean(axis=1)
+  result_train['xgb'] = result_xgb.mean(axis=1)
+
+  # result_train['nn'] = nn_prediction
+  # result_train['xgb'] = xgb_prediction
 
 clf = LinearRegression(n_jobs=-1)
 clf.fit(result_train, y_test)
@@ -83,11 +99,10 @@ result_test = pd.DataFrame()
 result_test['nn'] = nn_test['Hazard']
 result_test['xgb'] = xgb_test['Hazard']
 
-
 final_prediction = clf.predict(result_test)
 
 submission = pd.DataFrame()
 submission['Id'] = nn_test['Id']
 submission['Hazard'] = final_prediction
 
-submission.to_csv('linear/nn_xgb.csv', index=False)
+submission.to_csv('linear/nn_xgb_cv5.csv', index=False)
