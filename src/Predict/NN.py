fixed normalization for NN

src/Predict/NN.py
@@ -128,5 +128,11 @@ def NN(X_train, y_train, X_test, y_test):
 
   net1.fit(X, target.astype(np.float32))
 
-  return net1.predict(test)
+  def helper(x):
+    return (x * y_std) + y_mean
+
+  result = net1.predict(test)
+  result = np.reshape(result, len(y_test))
+
+  return map(helper, result)
 

src/find_weights.py
@@ -54,7 +54,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 [2] Do XGB and NN simulation on the train set and create prediction on the hold out set.
 '''
 
-nn_prediction = np.reshape(NN.NN(X_train, y_train, X_test, y_test), len(y_test))
+nn_prediction = NN.NN(X_train, y_train, X_test, y_test)
 xgb_prediction = XGB.XGB(X_train, y_train, X_test, y_test)
 
 