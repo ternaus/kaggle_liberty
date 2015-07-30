58d20e773fb841d46ad1ca312aea00409f0e4f5a
Vladimir Iglovikov <iglovikov@gmail.com>
2015-07-30 06:54:55 -0700

    change in order of predictions

--- src/find_weights.py
+++ src/find_weights.py
@@ -52,8 +52,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 [2] Do XGB and NN simulation on the train set and create prediction on the hold out set.
 '''
 
-xgb_prediction = XGB.XGB(X_train, y_train, X_test, y_test)
 nn_prediction = NN.NN(X_train, y_train, X_test, y_test)
+xgb_prediction = XGB.XGB(X_train, y_train, X_test, y_test)
+
 
 '''
 [3] Merge previous predictions into dataset and do linear regression on it
