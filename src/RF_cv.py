a5b3869e55f0b6e2f89014f11351b877dbb9d978
Vladimir Iglovikov <iglovikov@gmail.com>
2015-08-14 19:08:01 -0700

    new cv for RF added

--- src/RF_cv.py
+++ src/RF_cv.py
@@ -2,13 +2,13 @@ from __future__ import division
 __author__ = 'Vladimir Iglovikov'
 
 '''
-Here I will try to use xgb.
+Cross validation for RF
 '''
 import pandas as pd
 import xgboost as xgb
 from sklearn.cross_validation import ShuffleSplit
 import numpy as np
-
+from sklearn.ensemble import RandomForestRegressor
 
 joined = pd.read_csv('../data/joined.csv')
 
@@ -37,39 +37,18 @@ def normalized_gini(solution, submission):
 
 
 
-xgtest = xgb.DMatrix(X_test)
-
-
-params = {
-  'objective': 'reg:linear',
-  # 'eta': 0.005,
-  # 'min_child_weight': 6,
-  # 'subsample': 0.7,
-  # 'colsabsample_bytree': 0.7,
-  'scal_pos_weight': 1,
-  'silent': 1,
-  # 'max_depth': 9
-}
-
-num_rounds = 10000
 random_state = 42
 
 rs = ShuffleSplit(len(y), n_iter=10, test_size=0.5, random_state=random_state)
 
 result = []
 
-for min_child_weight in [9]:
-  for eta in [0.005]:
-    for colsample_bytree in [0.7]:
+for n_estimators in [10]:
+  for min_samples_split in [2]:
+    for max_features in [0.7]:
       for max_depth in [7]:
-        for subsample in [0.7]:
-          params['min_child_weight'] = min_child_weight
-          params['eta'] = eta
-          params['colsample_bytree'] = colsample_bytree
-          params['max_depth'] = max_depth
-          params['subsample'] = subsample
+        for min_samples_leaf in [1]:
           score = []
-          params_new = list(params.items())
           for train_index, test_index in rs:
 
             a_train = X.values[train_index]
@@ -77,18 +56,21 @@ for min_child_weight in [9]:
             b_train = y.values[train_index]
             b_test = y.values[test_index]
 
-            xgtrain = xgb.DMatrix(a_train, label=b_train)
-            xgval = xgb.DMatrix(a_test, label=b_test)
+            clf = RandomForestRegressor(n_estimators=n_estimators,
+                                        min_samples_split=min_samples_split,
+                                        max_features=max_features,
+                                        max_depth=max_depth,
+                                        min_samples_leaf=min_samples_leaf,
+                                        n_jobs=-1)
 
+            clf.fit(a_train, b_train)
 
-            watchlist = [(xgtrain, 'train'), (xgval, 'val')]
-            model = xgb.train(params_new, xgtrain, num_rounds, watchlist, early_stopping_rounds=120)
-            preds = model.predict(xgval, ntree_limit=model.best_iteration)
+            preds = clf.predict(a_test)
 
 
             score += [normalized_gini(b_test, preds)]
 
-          result += [(np.mean(score), np.std(score), min_child_weight, eta, colsample_bytree, max_depth, subsample)]
+          result += [(np.mean(score), np.std(score), n_estimators, min_samples_split, min_samples_leaf, max_depth, max_features)]
 
 result.sort()
 print result
